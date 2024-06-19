1488c06ca9ea4ee19f2ffa363d2dd625e70b9906
swe2mohamed <swe.mohamed1990@gmail.com>
2024-06-19 07:15:12 +0200

    4- Create Repository Package   a- Create UserRepository     - create two extra Query       . "updateExpiredByEmail"       . "updatePasswordByEmail"

---
+++ src/main/java/com/example/demo/Repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.Repository;
+
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Boolean existsByEmail(String email);
+
+    @Modifying
+    @Query("update User u set u.expired = :status where u.email= :email")
+    void updateExpiredByEmail(@Param("email") String email, @Param("status") boolean status);
+
+
+    @Modifying
+    @Query("update User u set u.password = :password where u.email = :email")
+    void updatePasswordByEmail(@Param("email") String email,@Param("password") String newPassword);
+}
